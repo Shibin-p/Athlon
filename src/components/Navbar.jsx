d148f21ac9f60f097c082aff23c51e71d0b038da
shibin_p <shibinp248@gmail.com>
2026-09-13 17:41:07 +0530

    change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ export function Navbar() {
                 ATHLON
               </span>
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-300">
-                26
+                '26
               </span>
             </span>
           </NavLink>
